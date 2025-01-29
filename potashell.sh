70fd8afbb0f447f8a818b09cf240b985fa48308c
Martin Francis <martin@classaxe.com>
2025-01-29 11:30:37 -0500

    1.10.2: Added potashell.sh to simplify use in Unix or on a Mac

---
+++ potashell.sh
@@ -0,0 +1,7 @@
+##############################
+# Potashell for Mac and Unix #
+##############################
+
+# To run potashell on a Mac or in Unix, call it like this: ./potashell.sh (arguments)
+
+php potashell.php $@
